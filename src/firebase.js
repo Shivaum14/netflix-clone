9536a540faf0a6c90fbe0d97c7c77a23cbacec8e
Shiv <Shivaumm@gmail.com>
2022-07-24 15:48:11 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyA_kZYOf-nwuI-2ZNB4w8SJBsvHje-d94A",
+	authDomain: "netflix-clone-b435f.firebaseapp.com",
+	projectId: "netflix-clone-b435f",
+	storageBucket: "netflix-clone-b435f.appspot.com",
+	messagingSenderId: "274040819208",
+	appId: "1:274040819208:web:dab7c62e985321c7850bbd",
+};
